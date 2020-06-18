cdc342889d828982fd3a238a4fb651b2e6f4d369
rafaelanderka <rafael.anderka@gmail.com>
2020-06-18 13:12:18 +0100

    Add basic circle shader for testing

---
+++ src/components/webgl/shaders/fragment/fs-circle.js
@@ -0,0 +1,20 @@
+export default `
+  // FRAGMENT SHADER
+
+  // Draws a circle for testing purposes
+
+  precision highp float;
+  precision highp sampler2D;
+
+  varying vec2 vUV; 
+  
+  void main(void) {
+    vec2 center = vec2(0.5, 0.5);
+    float dist = length(vUV - center);
+    if (dist < 0.1 && dist > 0.09) {
+      gl_FragColor = vec4(1.0);
+    } else {
+      gl_FragColor = vec4(0.0);
+    }
+  }
+`;
