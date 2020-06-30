ce2381ff1c17c3075dcd37bff53ed869700a69ea
rafaelanderka <rafael.anderka@gmail.com>
2020-06-30 13:55:36 +0100

    Fix bounce-back boundary condition

--- src/components/webgl/shaders/fragment/fs-streaming.js
+++ src/components/webgl/shaders/fragment/fs-streaming.js
@@ -1,7 +1,7 @@
 export default `
   // FRAGMENT SHADER
 
-  // Performs streaming
+  // Performs streaming and bounce-back
 
   precision highp float;
   precision highp sampler2D;
@@ -14,55 +14,80 @@ export default `
   
   void main(void) {
     // Determine whether node is adjacent to wall
-    int nodeIdN  = int(texture2D(uNodeId, vUV + vec2(            0, +uTexelSize.y)).x + 0.5);
-    int nodeIdNE = int(texture2D(uNodeId, vUV + vec2(+uTexelSize.x, +uTexelSize.y)).x + 0.5);
-    int nodeIdE  = int(texture2D(uNodeId, vUV + vec2(+uTexelSize.x,             0)).x + 0.5);
-    int nodeIdSE = int(texture2D(uNodeId, vUV + vec2(+uTexelSize.x, -uTexelSize.y)).x + 0.5);
-    int nodeIdS  = int(texture2D(uNodeId, vUV + vec2(            0, -uTexelSize.y)).x + 0.5);
-    int nodeIdSW = int(texture2D(uNodeId, vUV + vec2(-uTexelSize.x, -uTexelSize.y)).x + 0.5);
-    int nodeIdW  = int(texture2D(uNodeId, vUV + vec2(-uTexelSize.x,             0)).x + 0.5);
-    int nodeIdNW = int(texture2D(uNodeId, vUV + vec2(-uTexelSize.x, +uTexelSize.y)).x + 0.5);
-    bool isAdjacentToWall = nodeIdN == 1 || nodeIdNE == 1 || nodeIdE == 1 || nodeIdSE == 1 || nodeIdS == 1 || nodeIdSW == 1 || nodeIdW == 1 || nodeIdNW == 1;
+    bool wallN  = int(texture2D(uNodeId, vUV + vec2(            0, +uTexelSize.y)).x + 0.5) == 1;
+    bool wallNE = int(texture2D(uNodeId, vUV + vec2(+uTexelSize.x, +uTexelSize.y)).x + 0.5) == 1;
+    bool wallE  = int(texture2D(uNodeId, vUV + vec2(+uTexelSize.x,             0)).x + 0.5) == 1;
+    bool wallSE = int(texture2D(uNodeId, vUV + vec2(+uTexelSize.x, -uTexelSize.y)).x + 0.5) == 1;
+    bool wallS  = int(texture2D(uNodeId, vUV + vec2(            0, -uTexelSize.y)).x + 0.5) == 1;
+    bool wallSW = int(texture2D(uNodeId, vUV + vec2(-uTexelSize.x, -uTexelSize.y)).x + 0.5) == 1;
+    bool wallW  = int(texture2D(uNodeId, vUV + vec2(-uTexelSize.x,             0)).x + 0.5) == 1;
+    bool wallNW = int(texture2D(uNodeId, vUV + vec2(-uTexelSize.x, +uTexelSize.y)).x + 0.5) == 1;
+    bool isAdjacentToWall = wallN || wallNE || wallE || wallSE || wallS || wallSW || wallW || wallNW;
     
     float x = 0.0, y = 0.0, z = 0.0, w = 0.0;
-    if (isAdjacentToWall) {
-      // Bounce-back
-      vec4 nodalDistFunc = texture2D(uDistFunc, vUV);
-      #if defined(F0)
-      // Rest component
-      x = nodalDistFunc.x;
-      #elif defined(F1_4)
-      // Main cartesian components
+    vec4 nodalDistFunc = texture2D(uDistFunc, vUV);
+    #if defined(F0)
+    // Rest component
+    x = texture2D(uDistFunc, vUV).x;
+    #elif defined(F1_4)
+    // Main cartesian components
+    // East
+    if (wallW) {
       x = nodalDistFunc.z;
-      y = nodalDistFunc.w;
-      z = nodalDistFunc.x;
-      w = nodalDistFunc.y;
-      #elif defined(F5_8)
-      // Diagonal components
-      x = nodalDistFunc.z;
-      y = nodalDistFunc.w;
-      z = nodalDistFunc.x;
-      w = nodalDistFunc.y;
-      #endif
     } else {
-      // Regular streaming
-      #if defined(F0)
-      // Rest component
-      x = texture2D(uDistFunc, vUV).x;
-      #elif defined(F1_4)
-      // Main cartesian components
-      x = texture2D(uDistFunc, vUV + vec2(-uTexelSize.x,             0)).x;
-      y = texture2D(uDistFunc, vUV + vec2(            0, -uTexelSize.y)).y;
-      z = texture2D(uDistFunc, vUV + vec2(+uTexelSize.x,             0)).z;
-      w = texture2D(uDistFunc, vUV + vec2(            0, +uTexelSize.y)).w; 
-      #elif defined(F5_8)
-      // Diagonal components
-      x = texture2D(uDistFunc, vUV + vec2(-uTexelSize.x, -uTexelSize.y)).x;
-      y = texture2D(uDistFunc, vUV + vec2(+uTexelSize.x, -uTexelSize.y)).y;
-      z = texture2D(uDistFunc, vUV + vec2(+uTexelSize.x, +uTexelSize.y)).z;
-      w = texture2D(uDistFunc, vUV + vec2(-uTexelSize.x, +uTexelSize.y)).w;
-      #endif
+      x = texture2D(uDistFunc, vUV + vec2(-uTexelSize.x, 0)).x;
     }
+
+    // North
+    if (wallS) {
+      y = nodalDistFunc.w;
+    } else {
+      y = texture2D(uDistFunc, vUV + vec2(0, -uTexelSize.y)).y;
+    }
+
+    // West
+    if (wallE) {
+      z = nodalDistFunc.x;
+    } else {
+      z = texture2D(uDistFunc, vUV + vec2(+uTexelSize.x, 0)).z;
+    }
+
+    // South
+    if (wallN) {
+      w = nodalDistFunc.y;
+    } else {
+      w = texture2D(uDistFunc, vUV + vec2(0, +uTexelSize.y)).w; 
+    }
+    #elif defined(F5_8)
+    // Diagonal components
+    // Northeast
+    if (wallS || wallW || wallSW) {
+      x = nodalDistFunc.z;
+    } else {
+      x = texture2D(uDistFunc, vUV + vec2(-uTexelSize.x, -uTexelSize.y)).x;
+    }
+
+    // Northwest
+    if (wallS || wallE || wallSE) {
+      y = nodalDistFunc.w;
+    } else {
+      y = texture2D(uDistFunc, vUV + vec2(+uTexelSize.x, -uTexelSize.y)).y;
+    }
+
+    // Southwest
+    if (wallN || wallE || wallNE) {
+      z = nodalDistFunc.x;
+    } else {
+      z = texture2D(uDistFunc, vUV + vec2(+uTexelSize.x, +uTexelSize.y)).z;
+    }
+
+    // Southeast
+    if (wallN || wallW || wallNW) {
+      w = nodalDistFunc.y;
+    } else {
+      w = texture2D(uDistFunc, vUV + vec2(-uTexelSize.x, +uTexelSize.y)).w;
+    }
+    #endif
 
     gl_FragColor = vec4(x, y, z, w);
   }
